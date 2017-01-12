e021767387bbc83fb1194a88da9cc2a092c5995e
Derek Dick (RFS/RCS/PC <derek.dick@towerswatson.com>
2017-01-12 17:35:48 -0500

    add dynamic question panel.

--- complete/src/main/resources/static/app/home.component.ts
+++ complete/src/main/resources/static/app/home.component.ts
@@ -22,9 +22,9 @@ export class HomeComponent implements OnInit {
 
     constructor(private router:Router,
                 private formBuilder: FormBuilder,
-                private elementRef:ElementRef ) {
-               // private service: QuestionService) {
-        //this.questions = service.getQuestions();
+                private elementRef:ElementRef,
+                private service: QuestionService) {
+        this.questions = service.getQuestions();
     }
 
     ngOnInit() {
